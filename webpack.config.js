4ef1826f62ceb21a5e99fc80d7db50225ef134e1
Thach Nguyen <nthachus@gmail.com>
2020-08-26 16:11:24 +0700

    `yarn-deduplicate` CLI wrapper project

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,81 @@
 const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
-const CopyPkgJsonPlugin = require('copy-pkg-json-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const pkg = require('./package.json');
 
-const baseConfig = (name) => ({
-    mode: 'production',
-    entry: './' + name,
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: name + '.js',
-    },
-    name,
-    context: __dirname,
-    target: 'node',
-    node: {
-        __filename: false,
-        __dirname: false,
-    },
-    module: {
-        rules: [
-            {
-                // transpile ES6-8 into ES5
-                test: /\.m?js$/,
-                // exclude: /node_modules\b/,
-                loader: 'babel-loader',
-                options: {
-                    cacheDirectory: true,
-                    presets: [['@babel/preset-env', { targets: { node: '6' } }]], // esmodules
-                },
-            },
-        ],
-    },
-    optimization: {
-        nodeEnv: false,
-        // minimize: false,
-        minimizer: [
-            new TerserPlugin({
-                cache: true,
-                parallel: true,
-                terserOptions: {
-                    mangle: false,
-                    output: { beautify: true },
-                },
-            }),
-        ],
-    },
-});
+const patches = [
+  '\\bObject\\.keys\\(json\\)\\.forEach\\(.*?, packageName, requestedVersion\\] = match;',
+  `let resolvable = null;
+  if (includeScopes.length) {
+    includeScopes = includeScopes.filter(v => !['^', '~', '*'].includes(v) || !(resolvable = v));
+  }
+  $&
+  if (resolvable === '*') {
+    requestedVersion = '*';
+  } else if (resolvable === '^' && /(^|\\s|\\|)~\\s*\\d/.test(requestedVersion)) {
+    requestedVersion = requestedVersion.replace(/(^|\\s|\\|)~\\s*(\\d)/g, '$$1^$$2');
+  } else if (resolvable && /^\\d[\\w.-]*$$/.test(requestedVersion = semver.clean(requestedVersion))) {
+    requestedVersion = resolvable + requestedVersion.replace(/^(0)\\..*$$/, '$$1');
+  }`,
+];
 
-const config = [baseConfig('index'), baseConfig('cli')];
-
-Object.assign(config[0].output, {
-    libraryTarget: 'commonjs2',
-});
-
-Object.assign(config[1], {
-    externals: {
-        './index': 'commonjs2 ./index',
-    },
-    plugins: [
-        new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
-        new CopyPkgJsonPlugin({
-            remove: ['dependencies', 'devDependencies', 'scripts', 'engines', 'eslintConfig'],
-            replace: { engines: { node: '>=6' } },
-        }),
-        new CopyPlugin([{ from: '*.{txt,md,ts}' }]),
+module.exports = {
+  mode: 'production',
+  entry: './node_modules/yarn-deduplicate/cli',
+  output: {
+    path: __dirname,
+    filename: 'index.js',
+  },
+  context: __dirname,
+  target: 'node',
+  node: {
+    __filename: false,
+    __dirname: false,
+  },
+  // externals: [],
+  module: {
+    rules: [
+      {
+        // transpile ES6-8 into ES5
+        test: /\.m?js$/,
+        // exclude: /node_modules\b/,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          presets: [
+            ['@babel/preset-env', { targets: { node: pkg.engines.node.replace('>=', '') } }], // esmodules
+          ],
+        },
+      },
+      {
+        test: path.resolve(__dirname, 'node_modules/yarn-deduplicate/index.js'),
+        loader: 'string-replace-loader',
+        options: { search: patches[0], flags: 's', replace: patches[1] },
+      },
+      {
+        test: path.resolve(__dirname, 'node_modules/yarn-deduplicate/cli.js'),
+        loader: 'string-replace-loader',
+        options: {
+          multiple: [
+            { search: '^#!.*[\\r\\n]+', flags: '', replace: '' },
+            { search: "require('./package.json').version", replace: `'${pkg.version}'` },
+          ],
+        },
+      },
     ],
-});
-config[1].module.rules.unshift(
-    {
-        test: path.resolve(__dirname, 'cli.js'),
-        loader: 'string-replace-loader',
-        options: { search: '^#!.*[\\r\\n]+', flags: '', replace: '' },
-    },
-    {
-        test: path.resolve(__dirname, 'package.json'),
-        loader: 'string-replace-loader',
-        options: { search: ',\\s*"bin":.*$', flags: 's', replace: '}' },
-    }
-);
-
-module.exports = config;
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+  ],
+  optimization: {
+    nodeEnv: false,
+    // minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        terserOptions: { mangle: false, output: { beautify: true } },
+      }),
+    ],
+  },
+};
